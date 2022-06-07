21a2c3460cf2bfb26c6321cfbaf76cf7b6768d78
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-07 07:10:15 +0000

    產品pop框

---
+++ Vol.Vue3/src/extension/basic/prod/View_com_prod_pop_query.vue
@@ -0,0 +1,127 @@
+<template>
+    <VolBox
+            v-model="model"
+            :lazy="true"
+            title="产品pop框"
+            :height="450"
+            :width="800"
+            :padding="15"
+    >
+        <!-- 设置查询条件 -->
+        <div style="padding-bottom: 10px">
+            <span style="margin-right: 20px">请选择数据</span>
+            <el-input
+                    placeholder="ID"
+                    type="like"
+                    style="width: 200px"
+                    v-model="prod_id"
+            />
+            <el-input
+                    placeholder="EnglishName"
+                    type="like"
+                    style="width: 200px"
+                    v-model="prod_ename"
+            />
+            <el-input
+                    placeholder="ChineseName"
+                    type="like"
+                    style="width: 200px"
+                    v-model="prod_sname"
+            />
+            <el-button
+                    type="primary"
+                    style="margin-left:10px"
+                    size="medium"
+                    icon="el-icon-zoom-out"
+                    @click="search"
+            >搜索</el-button
+            >
+        </div>
+
+        <!-- vol-table配置的这些属性见VolTable组件api文件 -->
+        <vol-table
+                ref="prodPop"
+                :loadKey="true"
+                :columns="columns"
+                :pagination="pagination"
+                :pagination-hide="false"
+                :max-height="380"
+                :url="url"
+                :index="true"
+                :single="true"
+                :defaultLoadPage="defaultLoadPage"
+                @loadBefore="loadTableBefore"
+                @loadAfter="loadTableAfter"
+        ></vol-table>
+        <!-- 设置弹出框的操作按钮 -->
+        <template #footer>
+            <div>
+                <el-button
+                        size="mini"
+                        type="primary"
+                        icon="el-icon-plus"
+                        @click="addRow()"
+                >确定</el-button
+                >
+                <el-button size="mini" icon="el-icon-close" @click="model = false"
+                >关闭</el-button
+                >
+            </div>
+        </template>
+    </VolBox>
+</template>
+<script>
+    import VolBox from "@/components/basic/VolBox.vue";
+    import VolTable from "@/components/basic/VolTable.vue";
+    export default {
+        components: {
+            VolBox: VolBox,
+            VolTable: VolTable,
+        },
+        data() {
+            return {
+                model: false,
+                defaultLoadPage: false, //第一次打开时不加载table数据，openDemo手动调用查询table数据
+                cust_name: "", //查询条件字段
+                fieldName:"",//編輯字段,用於回傳設置值
+                url: "api/View_com_prod_pop_query/getPageData",//加载数据的接口
+                columns: [
+                    {field:'mpg_id',title:'Mpg',type:'string',width:90,align:'left',sort:true},
+                    {field:'localmpg_dbid',title:'localmpg_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                    {field:'prod_id',title:'PID',type:'string',width:90,require:true,align:'left'},
+                    {field:'prod_ename',title:'E-Name',type:'string',width:130,align:'left'},
+                    {field:'prod_cname',title:'prod_cname',type:'string',width:110,hidden:true,align:'left'},
+                    {field:'prod_sname',title:'C-Name',type:'string',width:130,align:'left'},
+                    {field:'nhi_price',title:'NHI Price',type:'decimal',width:90,align:'left'},
+                    {field:'pack_size',title:'Pack Size',type:'decimal',width:90,align:'left'},
+                    {field:'globalmpg_dbid',title:'globalmpg_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                    {field:'created_date1',title:'created_date1',type:'datetime',width:150,hidden:true,align:'left',sort:true},
+                    {field:'prod_dbid',title:'prod_dbid',type:'guid',width:110,hidden:true,require:true,align:'left'}],
+            };
+        },
+        methods: {
+            open() {
+                this.model = true;
+
+                //打开弹出框时，加载table数据
+                this.$nextTick(() => {
+                    this.$refs.prodPop.load();
+                });
+            },
+
+            addRow() {
+                let selectrow = this.$refs.prod.$refs.grid.getSelectRows();
+                if(!selectrow.length){
+                    return this.$message.error("请选择数据")
+                }
+                //将选取的数据赋值到父页面
+                this.$emit('parentCall', $parent => {
+                    $parent.editFormFields.prod_sname = selectrow[0].prod_sname;
+                    this.model=false;
+                })
+
+            },
+
+        },
+    };
+</script>
---
+++ Vol.Vue3/src/views/basic/prod/View_com_prod_pop_query.vue
@@ -0,0 +1,72 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/prod/View_com_prod_pop_query.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+
+
+
+</template>
+<script>
+    import extend from "@/extension/basic/prod/View_com_prod_pop_query.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'prod_dbid',
+                footer: "Foots",
+                cnName: '產品pop框查詢',
+                name: 'prod/View_com_prod_pop_query',
+                url: "/View_com_prod_pop_query/",
+                sortName: "created_date1"
+            });
+            const editFormFields = ref({});
+            const editFormOptions = ref([]);
+            const searchFormFields = ref({"prod_id":"","prod_ename":"","prod_sname":""});
+            const searchFormOptions = ref([[{"title":"ID","field":"prod_id","type":"like"},{"title":"EnglishName","field":"prod_ename"},{"title":"ChineseName","field":"prod_sname","type":"like"}]]);
+            const columns = ref([{field:'mpg_id',title:'Mpg',type:'string',width:90,align:'left',sort:true},
+                       {field:'localmpg_dbid',title:'localmpg_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                       {field:'prod_id',title:'PID',type:'string',width:90,require:true,align:'left'},
+                       {field:'prod_ename',title:'E-Name',type:'string',width:130,align:'left'},
+                       {field:'prod_cname',title:'prod_cname',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'prod_sname',title:'C-Name',type:'string',width:130,align:'left'},
+                       {field:'nhi_price',title:'NHI Price',type:'decimal',width:90,align:'left'},
+                       {field:'pack_size',title:'Pack Size',type:'decimal',width:90,align:'left'},
+                       {field:'globalmpg_dbid',title:'globalmpg_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                       {field:'created_date1',title:'created_date1',type:'datetime',width:150,hidden:true,align:'left',sort:true},
+                       {field:'prod_dbid',title:'prod_dbid',type:'guid',width:110,hidden:true,require:true,align:'left'}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+        methods: {
+
+        },
+    });
+</script>
