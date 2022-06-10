eb65acf13ab3d5ab661e682ee9e1774d93358852
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-10 01:25:28 +0000

    新增代理人

---
+++ src/views/basic/deputy/Viat_sys_deputy.vue
@@ -0,0 +1,80 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/deputy/Viat_sys_deputy.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/deputy/Viat_sys_deputy.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'deputy_dbid',
+                footer: "Foots",
+                cnName: '代理人清單',
+                name: 'deputy/Viat_sys_deputy',
+                url: "/Viat_sys_deputy/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"deputy_dbid":"","dbid":"","user_id":"","deputy_user_id":"","start_date":"","end_date":"","status":""});
+            const editFormOptions = ref([[{"title":"PKID","required":true,"field":"deputy_dbid","disabled":true},
+                               {"title":"識別碼, PK, Identity","required":true,"field":"dbid","type":"number"},
+                               {"title":"用戶表PKI","required":true,"field":"user_id","type":"number"},
+                               {"title":"代理人用戶PKID","required":true,"field":"deputy_user_id","type":"number"},
+                               {"title":"代理起始日","required":true,"field":"start_date"},
+                               {"title":"代理結束日","required":true,"field":"end_date"},
+                               {"title":"是否有效,Y:生效/N:未生效or期滿/C:移除","required":true,"field":"status"}]]);
+            const searchFormFields = ref({});
+            const searchFormOptions = ref([]);
+            const columns = ref([{field:'deputy_dbid',title:'PKID',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
+                       {field:'dbid',title:'識別碼, PK, Identity',type:'int',width:110,require:true,align:'left',sort:true},
+                       {field:'entity',title:'公司別,舊版SUN_DB',type:'string',width:110,align:'left'},
+                       {field:'division',title:'所屬事業單位,01:PH;03:AH;05:CH;06:NU',type:'string',width:110,align:'left'},
+                       {field:'user_id',title:'用戶表PKI',type:'int',width:110,require:true,align:'left'},
+                       {field:'deputy_user_id',title:'代理人用戶PKID',type:'int',width:110,require:true,align:'left'},
+                       {field:'start_date',title:'代理起始日',type:'datetime',width:110,require:true,align:'left',sort:true},
+                       {field:'end_date',title:'代理結束日',type:'datetime',width:110,require:true,align:'left',sort:true},
+                       {field:'status',title:'是否有效,Y:生效/N:未生效or期滿/C:移除',type:'string',width:110,require:true,align:'left'},
+                       {field:'created_user',title:'建立用戶',type:'int',width:110,align:'left'},
+                       {field:'created_username',title:'created_username',type:'string',width:110,align:'left'},
+                       {field:'created_client',title:'建立者的委託人',type:'int',width:110,align:'left'},
+                       {field:'created_clientusername',title:'created_clientusername',type:'string',width:110,align:'left'},
+                       {field:'created_date',title:'建立時間',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'modified_user',title:'最後修改用戶',type:'int',width:110,align:'left'},
+                       {field:'modified_username',title:'modified_username',type:'string',width:110,align:'left'},
+                       {field:'modified_client',title:'最後修改者的委託人',type:'int',width:110,align:'left'},
+                       {field:'modified_clientusername',title:'modified_clientusername',type:'string',width:110,align:'left'},
+                       {field:'modified_date',title:'最後修改時間',type:'datetime',width:110,align:'left',sort:true}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
---
+++ src/views/basic/deputy/View_sys_deputy.vue
@@ -0,0 +1,78 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/deputy/View_sys_deputy.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/deputy/View_sys_deputy.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'deputy_dbid',
+                footer: "Foots",
+                cnName: '代理信息',
+                name: 'deputy/View_sys_deputy',
+                url: "/View_sys_deputy/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"user_name2":"","UserTrueName":"","emp_ename":"","emp_cname":"","start_date":"","end_date":"","deputy_user_id":"","user_id2":"","status":""});
+            const editFormOptions = ref([[{"title":"Login User","required":true,"field":"user_name2"},
+                               {"title":"","required":true,"field":"UserTrueName"}],
+                              [{"title":"Deputy Name","field":"emp_ename"},
+                               {"title":"","field":"emp_cname"}],
+                              [{"title":"Start Date","required":true,"field":"start_date","type":"datetime"}],
+                              [{"title":"End Date","required":true,"field":"end_date","type":"datetime"}],
+                              [{"title":"Deputy Id","required":true,"field":"deputy_user_id",hidden:true,"type":"number"},
+                               {"title":"User Id","required":true,"field":"user_id2",hidden:true,"type":"number"},
+                               {"title":"status",hidden:true,"field":"status"}]]);
+            const searchFormFields = ref({"start_date":"","end_date":""});
+            const searchFormOptions = ref([[{"dataKey":"deputyDate","data":[],"title":"Deputy Date","field":"deputyDate","type":"select"},
+                {"title":"","field":"start_date","type":"datetime"},
+                {"title":"","field":"end_date","type":"datetime"}]]);
+            const columns = ref([{field:'deputy_dbid',title:'deputy_dbid',type:'guid',width:110,hidden:true,require:true,align:'left'},
+                       {field:'start_date',title:'Start Date',type:'datetime',width:130,require:true,align:'left',sort:true},
+                       {field:'end_date',title:'End Date',type:'datetime',width:130,require:true,align:'left',sort:true},
+                       {field:'deputy_user_id',title:'Deputy Id',type:'int',width:90,require:true,align:'left'},
+                       {field:'emp_ename',title:'Deputy Name',type:'string',width:90,align:'left'},
+                       {field:'user_id2',title:'User Id',type:'int',width:90,require:true,align:'left'},
+                       {field:'user_name2',title:'User Name',type:'string',width:110,require:true,align:'left'},
+                       {field:'user_id',title:'列userid',type:'int',width:90,hidden:true,require:true,align:'left'},
+                       {field:'created_date',title:'created_date',type:'datetime',width:150,hidden:true,align:'left',sort:true},
+                       {field:'dept_name',title:'dept_name',type:'string',width:120,hidden:true,align:'left'},
+                       {field:'emp_cname',title:'emp_cname',type:'string',width:120,hidden:true,align:'left'},
+                       {field:'UserTrueName',title:'UserTrueName',type:'string',width:110,hidden:true,require:true,align:'left'},
+                       {field:'status',title:'status',type:'string',width:110,hidden:true,require:true,align:'left'}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
