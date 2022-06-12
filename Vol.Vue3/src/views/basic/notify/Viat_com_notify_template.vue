827b047be385a05d1f2071068913a089173eb907
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-12 09:56:06 +0000

    消息模板

---
+++ Vol.Vue3/src/views/basic/notify/Viat_com_notify_template.vue
@@ -0,0 +1,87 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/notify/Viat_com_notify_template.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/notify/Viat_com_notify_template.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'notifytemp_dbid',
+                footer: "Foots",
+                cnName: '消息模板',
+                name: 'notify/Viat_com_notify_template',
+                url: "/Viat_com_notify_template/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"entity":"","notify_id":"","sender":"","recipient":"","bcc":"","cc":"","subject":"","content":"","remarks":"","modified_user":"","modified_username":"","modified_date":""});
+            const editFormOptions = ref([[{"title":"公司別,舊版SUN_DB","field":"entity"},
+                               {"title":"範本自訂代碼","required":true,"field":"notify_id"},
+                               {"title":"寄件者","field":"sender"},
+                               {"title":"收件者","field":"recipient"},
+                               {"title":"密件收件者","field":"bcc"},
+                               {"title":"副本收件者","field":"cc"},
+                               {"title":"主旨","field":"subject"},
+                               {"title":"內容","field":"content"},
+                               {"title":"備註","field":"remarks"},
+                               {"title":"最後修改用戶","field":"modified_user","type":"number"},
+                               {"title":"","field":"modified_username"},
+                               {"title":"最後修改時間","field":"modified_date"}]]);
+            const searchFormFields = ref({});
+            const searchFormOptions = ref([]);
+            const columns = ref([{field:'notifytemp_dbid',title:'PKID',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
+                       {field:'entity',title:'公司別,舊版SUN_DB',type:'string',width:110,align:'left',sort:true},
+                       {field:'notify_id',title:'範本自訂代碼',type:'string',width:110,require:true,align:'left'},
+                       {field:'function_id',title:'模組自訂代碼',type:'string',width:110,align:'left'},
+                       {field:'sender',title:'寄件者',type:'string',width:180,align:'left'},
+                       {field:'recipient',title:'收件者',type:'string',width:180,align:'left'},
+                       {field:'bcc',title:'密件收件者',type:'string',width:180,align:'left'},
+                       {field:'cc',title:'副本收件者',type:'string',width:180,align:'left'},
+                       {field:'subject',title:'主旨',type:'string',width:180,align:'left'},
+                       {field:'content',title:'內容',type:'string',width:220,align:'left'},
+                       {field:'remarks',title:'備註',type:'string',width:220,align:'left'},
+                       {field:'created_user',title:'建立用戶',type:'int',width:110,align:'left'},
+                       {field:'created_username',title:'created_username',type:'string',width:110,align:'left'},
+                       {field:'created_client',title:'建立者的委託人',type:'int',width:110,align:'left'},
+                       {field:'created_clientusername',title:'created_clientusername',type:'string',width:110,align:'left'},
+                       {field:'created_date',title:'建立時間',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'modified_user',title:'最後修改用戶',type:'int',width:110,align:'left'},
+                       {field:'modified_username',title:'modified_username',type:'string',width:110,align:'left'},
+                       {field:'modified_client',title:'最後修改者的委託人',type:'int',width:110,align:'left'},
+                       {field:'modified_clientusername',title:'modified_clientusername',type:'string',width:110,align:'left'},
+                       {field:'modified_date',title:'最後修改時間',type:'datetime',width:110,align:'left',sort:true}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
---
+++ Vol.Vue3/src/views/basic/notify/View_com_notify_template.vue
@@ -0,0 +1,82 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/notify/View_com_notify_template.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/notify/View_com_notify_template.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'notifytemp_dbid',
+                footer: "Foots",
+                cnName: '消息模板設置',
+                name: 'notify/View_com_notify_template',
+                url: "/View_com_notify_template/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"notify_id":"","sender":"","subject":"","recipient":"","cc":"","bcc":"","content":"","remarks":"","emp_ename":"","modified_date":""});
+            const editFormOptions = ref([[{"title":"Notify ID","required":true,"field":"notify_id"},
+                               {"title":"Sender Nickname","required":true,"field":"sender"}],
+                              [{"title":"Subject","required":true,"field":"subject"}],
+                              [{"title":"Recipient","field":"recipient"}],
+                              [{"title":"Cc","field":"cc"}],
+                              [{"title":"Bcc","field":"bcc"}],
+                              [{"title":"Content","field":"content","type":"textarea"}],
+                              [{"title":"Remarks","field":"remarks"}],
+                              [{"title":"Modified User","field":"emp_ename","disabled":true},
+                               {"title":"Modified Date","field":"modified_date","disabled":true}]]);
+            const searchFormFields = ref({"notify_id":"","subject":""});
+            const searchFormOptions = ref([[{"title":"Notify ID","field":"notify_id","type":"like"},{"title":"Subject","field":"subject","type":"like"}]]);
+            const columns = ref([{field:'notifytemp_dbid',title:'列名notifytemp_dbid',type:'guid',width:110,hidden:true,require:true,align:'left'},
+                       {field:'created_date',title:'列名created_date',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'modified_user',title:'列名modified_user',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'notify_id',title:'Notify ID',type:'string',link:true,width:110,require:true,align:'left',sort:true},
+                       {field:'sender',title:'Sender',type:'string',width:110,align:'left'},
+                       {field:'subject',title:'Subject',type:'string',width:180,align:'left'},
+                       {field:'entity',title:'列名entity',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'recipient',title:'Recipient',type:'string',width:180,align:'left'},
+                       {field:'cc',title:'Cc',type:'string',width:110,align:'left'},
+                       {field:'bcc',title:'Bcc',type:'string',width:110,align:'left'},
+                       {field:'remarks',title:'Remarks',type:'string',width:110,align:'left'},
+                       {field:'modified_username',title:'列名modified_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'emp_ename',title:'Last Modified',type:'string',width:110,readonly:true,align:'left'},
+                       {field:'modified_date',title:'Modified Date',type:'datetime',width:110,readonly:true,align:'left',sort:true},
+                       {field:'function_id',title:'列名function_id',type:'string',width:110,hidden:true,require:true,align:'left'},
+                       {field:'content',title:'列名content',type:'string',width:220,hidden:true,align:'left'},
+                       {field:'emp_cname',title:'列名emp_cname',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'row_number',title:'列名row_number',type:'long',width:110,hidden:true,align:'left'}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
