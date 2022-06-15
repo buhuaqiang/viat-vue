4899cf6052a3feb1eb17f595b2c3320ae33079c1
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-15 02:20:58 +0000

    系統訊息設置

---
+++ Vol.Vue3/src/views/basic/bulletin/Viat_app_bulletin_receiver.vue
@@ -0,0 +1,69 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/bulletin/Viat_app_bulletin_receiver.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/bulletin/Viat_app_bulletin_receiver.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'receiver_dbid',
+                footer: "Foots",
+                cnName: '系統訊息接收者',
+                name: 'bulletin/Viat_app_bulletin_receiver',
+                url: "/Viat_app_bulletin_receiver/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({});
+            const editFormOptions = ref([]);
+            const searchFormFields = ref({});
+            const searchFormOptions = ref([]);
+            const columns = ref([{field:'receiver_dbid',title:'receiver_dbid',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
+                       {field:'bulletin_dbid',title:'PKID',type:'guid',width:110,align:'left',sort:true},
+                       {field:'user_id',title:'user id',type:'int',width:110,align:'left'},
+                       {field:'status',title:'讀取狀態,0:未讀;1:已讀;2:刪除',type:'string',width:110,align:'left'},
+                       {field:'created_user',title:'建立用戶',type:'int',width:110,align:'left'},
+                       {field:'created_username',title:'created_username',type:'string',width:110,align:'left'},
+                       {field:'created_client',title:'建立者的委託人',type:'int',width:110,align:'left'},
+                       {field:'created_clientusername',title:'created_clientusername',type:'string',width:110,align:'left'},
+                       {field:'created_date',title:'建立時間',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'modified_user',title:'最後修改用戶',type:'int',width:110,align:'left'},
+                       {field:'modified_username',title:'modified_username',type:'string',width:110,align:'left'},
+                       {field:'modified_client',title:'最後修改者的委託人',type:'int',width:110,align:'left'},
+                       {field:'modified_clientusername',title:'modified_clientusername',type:'string',width:110,align:'left'},
+                       {field:'modified_date',title:'最後修改時間',type:'datetime',width:110,align:'left',sort:true}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
---
+++ Vol.Vue3/src/views/basic/bulletin/View_com_bulletin.vue
@@ -0,0 +1,93 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/bulletin/View_com_bulletin.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/bulletin/View_com_bulletin.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'bulletin_dbid',
+                footer: "Foots",
+                cnName: '系統訊息設置',
+                name: 'bulletin/View_com_bulletin',
+                url: "/View_com_bulletin/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"subject":"","content":"","start_date":"","end_date":"","send":"","users":"","user":"","SelectUsers":"","modified_date":"","emp_ename":""});
+            const editFormOptions = ref([[{"title":"Subject","field":"subject","required":true}],
+                              [{"title":"Content","field":"content","required":true}],
+                              [{"title":"Publish Date","field":"start_date","type":"date","required":true},
+                               {"title":"","field":"end_date","type":"date"}],
+                              [{"dataKey":"SendTo","data":[],"title":"Send To","required":true,"field":"send","type":"radio"}],
+                //[{"title":"Users","required":true,"field":"users"}],
+                              [{"dataKey":"SendTo","data":[],"title":"Users","field":"users","type":"selectList"},
+                               {"title":"Users","field":"user","disabled":true}],
+                              [{"title":"SelectUsers","field":"SelectUsers",hidden:true}],
+                              [{"title":"Last Modified","field":"emp_ename","disabled":true},
+                               {"title":"Modified Date","field":"modified_date","disabled":true}]]);
+            const searchFormFields = ref({"start_date":"","end_date":""});
+            const searchFormOptions = ref([[{"title":"Start Date","field":"start_date","type":"date"},{"title":"End Date","field":"end_date","type":"date"}]]);
+            const columns = ref([{field:'user_id',title:'列名user_id',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'bulletin_dbid',title:'列名bulletin_dbid',type:'guid',width:110,hidden:true,require:true,align:'left'},
+                       {field:'C1',title:'列名C1',type:'int',width:110,hidden:true,require:true,align:'left'},
+                       {field:'created_user',title:'列名created_user',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_client',title:'列名created_client',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_date',title:'列名created_date',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'modified_user',title:'列名modified_user',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_client',title:'列名modified_client',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'entity',title:'列名entity',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_username',title:'Creator',type:'string',width:110,align:'left'},
+                       {field:'subject',title:'Subject',type:'string',link:true,width:120,align:'left'},
+                       {field:'content',title:'Content',type:'string',width:220,align:'left'},
+                       {field:'start_date',title:'Start Date',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'end_date',title:'End Date',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'type',title:'列名type',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'emp_cname',title:'列名emp_cname',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'emp_ename',title:'Last Modified',type:'string',width:110,readonly:true,align:'left'},
+                       {field:'C2',title:'列名C2',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_user1',title:'列名created_user1',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_client1',title:'列名created_client1',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_date1',title:'列名created_date1',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'modified_user1',title:'列名modified_user1',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_client1',title:'列名modified_client1',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_date1',title:'列名modified_date1',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'user_id1',title:'列名user_id1',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'status',title:'列名status',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'modified_date',title:'Modified Date',type:'datetime',width:110,readonly:true,align:'left',sort:true},
+                       {field:'send',title:'Send To',type:'int',bind:{ key:'SendTo',data:[]},width:80,hidden:true,require:true,align:'left'}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
