976b8b7cd6619cda95c276efce4aceb2084a9320
jo <jo@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-17 03:54:50 +0000

    Dist margin rate

---
+++ Vol.Vue3/src/extension/basic/distroub/Viat_app_dist_marginModelBody.vue
@@ -0,0 +1,133 @@
+<template>
+    <VolBox
+            v-model="model"
+            :lazy="true"
+            title="选择数据"
+            :height="600"
+            :width="1150"
+            :padding="15"
+    >
+        <!-- 设置查询条件 -->
+        <div style="padding-bottom: 10px">
+            <span style="margin-right: 20px">请选择数据</span>
+            <el-input
+
+                    placeholder="名称"
+                    style="width: 200px"
+                    v-model="cust_name"
+            />
+            <el-button
+                    type="primary"
+                    style="margin-left:10px"
+                    size="medium"
+                    icon="el-icon-zoom-out"
+                    @click="search"
+            >搜索</el-button
+            >
+        </div>
+
+        <!-- vol-table配置的这些属性见VolTable组件api文件 -->
+        <vol-table
+                ref="mytable"
+                :loadKey="true"
+                :columns="columns"
+                :pagination="pagination"
+                :pagination-hide="false"
+                :max-height="420"
+                :url="url"
+                :index="true"
+                :single="true"
+                :defaultLoadPage="defaultLoadPage"
+                @loadBefore="loadTableBefore"
+                @loadAfter="loadTableAfter"
+        ></vol-table>
+        <!-- 设置弹出框的操作按钮 -->
+        <template #footer>
+            <div>
+                <el-button
+                        size="mini"
+                        type="primary"
+                        icon="el-icon-plus"
+                        @click="addRow()"
+                >添加选择的数据</el-button
+                >
+                <el-button size="mini" icon="el-icon-close" @click="model = false"
+                >关闭</el-button
+                >
+            </div>
+        </template>
+    </VolBox>
+</template>
+<script>
+    import VolBox from "@/components/basic/VolBox.vue";
+    import VolTable from "@/components/basic/VolTable.vue"
+    export default {
+        components: {
+            VolBox: VolBox,
+            VolTable: VolTable,
+        },
+        data() {
+            return {
+                model: false,
+                defaultLoadPage: false, //第一次打开时不加载table数据，openDemo手动调用查询table数据
+                cust_name: "", //查询条件字段
+                fieldName:"",//編輯字段,用於回傳設置值
+                url: "api/View_com_cust/GetPopPageData",//加载数据的接口
+                columns: [
+                    {field:'cust_id',title:'客戶編號',type:'string',width:110,require:true,align:'left',sort:true},
+                    {field:'cust_name',title:'客戶名',type:'string',link:true,width:120,align:'left'},
+                    {field:'cust_address',title:'客戶地址',type:'string',width:180,align:'left'},
+                    {field:'tel_no',title:'聯繫電話',type:'string',width:110,align:'left'},
+                    {field:'territory_id',title:'默認區域',type:'string',width:110,hidden:true,align:'left'},
+                    {field:'doh_type',title:'健保類別',type:'string',bind:{ key:'doh_type',data:[]},width:110,align:'left'},
+                    {field:'margin_type',title:'毛利類別',type:'string',bind:{ key:'doh_type',data:[]},width:110,align:'left'},
+                    {field:'status',title:'status',type:'string',bind:{ key:'Status2',data:[]},width:110,align:'left'},
+                    {field:'modified_date',title:'最后修改时间',type:'datetime',width:150,align:'left',sort:true}
+                ],
+                pagination: {}, //分页配置，见voltable组件api
+            };
+        },
+        methods: {
+            openDemo(fieldName) {
+                this.model = true;
+                this.fieldName=fieldName;
+                //打开弹出框时，加载table数据
+                this.$nextTick(() => {
+                    this.$refs.mytable.load();
+                });
+            },
+            search() {
+                //点击搜索
+                this.$refs.mytable.load();
+            },
+            addRow() {
+                var rows = this.$refs.mytable.getSelected();
+                if (!rows || rows.length == 0) {
+                    return this.$message.error("请选择行数据");
+                }
+                //回写数据到表单
+                this.$emit("parentCall", ($parent) => {
+                    //将选择的数据合并到表单中(注意框架生成的代码都是大写，后台自己写的接口是小写的)
+                    $parent.editFormFields[this.fieldName] = rows[0].cust_dbid;
+                    $parent.editFormFields[this.fieldName+'name'] = rows[0].cust_id+" "+rows[0].cust_name;
+                });
+                //关闭当前窗口
+                this.model = false;
+            },
+            //这里是从api查询后返回数据的方法
+            loadTableAfter(row) {
+                //   let url="";
+                // this.http.get(url, params, true).then((result) => {
+                //     this.da
+                // });
+            },
+            loadTableBefore(params) {
+                //查询前，设置查询条件
+                if (this.cust_name) {
+                    params.wheres = [{ name: "cust_name", value: this.cust_name }];
+                }
+                return true;
+            },
+        },
+    };
+</script>
---
+++ Vol.Vue3/src/views/basic/distroub/Viat_app_dist_margin.vue
@@ -0,0 +1,88 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/distroub/Viat_app_dist_margin.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/basic/distroub/Viat_app_dist_margin.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'distmargin_dbid',
+                footer: "Foots",
+                cnName: '經銷商margin設定',
+                name: 'distroub/Viat_app_dist_margin',
+                url: "/Viat_app_dist_margin/",
+                sortName: "distmargin_dbid"
+            });
+            const editFormFields = ref({"dist_id":"","margin_value_type":"","margin_value":"","start_date":"","end_date":"","status":"","channel":"","cust_dbid":"","prod_dbid":""});
+            const editFormOptions = ref([[{"dataKey":"distributor_Drop","data":[],"title":"經銷商代碼","required":true,"field":"dist_id","type":"select"}],
+                              [{"dataKey":"Margin_Type","data":[],"title":"毛利類別,0: Fix Rate ;1:Fix Price","field":"margin_value_type","type":"select"}],
+                              [{"title":"毛利值,Rate or 固定金額","field":"margin_value","type":"text"}],
+                              [{"title":"起始日期","field":"start_date","type":"datetime"}],
+                              [{"title":"結束日期","field":"end_date","type":"datetime"}],
+                              [{"dataKey":"status-Dist2","data":[],"title":"Y:是,N：否","field":"status","type":"select"}],
+                              [{"dataKey":"Channel-Distributor2","data":[],"title":"通路","field":"channel","type":"select"}],
+                              [{"title":"客戶代碼","field":"cust_dbid","type":"text"}],
+                              [{"title":"產品代碼,舊版ITEM_CODE","field":"prod_dbid","type":"text"}]]);
+            const searchFormFields = ref({});
+            const searchFormOptions = ref([]);
+            const columns = ref([{field:'distmargin_dbid',title:'PKID',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
+                       {field:'dbid',title:'識別碼,  Identity',type:'int',width:110,require:true,align:'left',sort:true},
+                       {field:'entity',title:'公司別',type:'string',width:110,align:'left'},
+                       {field:'division',title:'所屬事業單位,01:PH;03:AH;05:CH;06:NU',type:'string',width:110,align:'left'},
+                       {field:'dist_id',title:'經銷商代碼',type:'string',bind:{ key:'distributor_Drop',data:[]},width:110,require:true,align:'left'},
+                       {field:'margin_value_type',title:'毛利類別,0: Fix Rate ;1:Fix Price',type:'int',bind:{ key:'Margin_Type',data:[]},width:110,align:'left'},
+                       {field:'margin_value',title:'毛利值,Rate or 固定金額',type:'decimal',width:110,align:'left'},
+                       {field:'prod_dbid',title:'產品代碼,舊版ITEM_CODE',type:'guid',width:110,align:'left'},
+                       {field:'cust_dbid',title:'客戶代碼',type:'guid',width:110,align:'left'},
+                       {field:'local_mpg_dbid',title:'Local MPG ID',type:'guid',width:110,align:'left'},
+                       {field:'channel',title:'通路',type:'string',bind:{ key:'Channel-Distributor2',data:[]},width:110,align:'left'},
+                       {field:'start_date',title:'起始日期',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'end_date',title:'結束日期',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'remarks',title:'備註',type:'string',width:220,align:'left'},
+                       {field:'status',title:'Y:是,N：否',type:'string',bind:{ key:'status-Dist2',data:[]},width:110,align:'left'},
+                       {field:'created_user',title:'建立用戶',type:'int',width:110,align:'left'},
+                       {field:'created_username',title:'created_username',type:'string',width:110,align:'left'},
+                       {field:'created_client',title:'建立者的委託人',type:'int',width:110,align:'left'},
+                       {field:'created_clientusername',title:'created_clientusername',type:'string',width:110,align:'left'},
+                       {field:'created_date',title:'建立時間',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'modified_user',title:'最後修改用戶',type:'int',width:110,align:'left'},
+                       {field:'modified_username',title:'modified_username',type:'string',width:110,align:'left'},
+                       {field:'modified_client',title:'最後修改者的委託人',type:'int',width:110,align:'left'},
+                       {field:'modified_clientusername',title:'modified_clientusername',type:'string',width:110,align:'left'},
+                       {field:'modified_date',title:'最後修改時間',type:'datetime',width:110,align:'left',sort:true}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
