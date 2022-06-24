81199ca91bac360170d28dd4121a905933011a91
bu <bu@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-24 03:07:12 +0000

    OKTA

---
+++ Vol.Vue3/src/components/Home.vue
@@ -0,0 +1,73 @@
+
+
+<template>
+  <div id="home">
+
+
+  </div>
+</template>
+
+<script>
+import store from "../store";
+import {getCurrentInstance, reactive} from "vue";
+import http from "@/../src/api/http.js";
+import {useRouter} from "vue-router";
+
+export default {
+  name: 'home',
+  data: function () {
+    return {
+      claims: '',
+    }
+  },
+  mounted () { this.setup() },
+  methods: {
+    async setup () {
+
+      const userInfo = reactive({
+        userName: "",
+        password: "",
+        verificationCode: "",
+        UUID: undefined,
+      });
+      let appContext = getCurrentInstance().appContext;
+      let $message = appContext.config.globalProperties.$message;
+      let router = useRouter();
+    //  alert(store.getters.isLoading());
+      let loginOut = this.$route.query.loginOut;
+      if(loginOut){
+        this.logout();
+      }
+      else if(this.authState?.isAuthenticated ) {
+        this.claims = await this.$auth.getUser()
+         userInfo.userName=this.claims.email
+       // userInfo.userName = 'admin'
+        userInfo.password="9999999  "
+        userInfo.verificationCode="0000"
+        userInfo.UUID="9999";
+        http.post("/api/user/login?bverificationCode=false", userInfo, "正在登錄....").then((result) => {
+          if (!result.status) {
+            //loading.value = false;
+            return $message.error(result.message);
+          }
+          $message.success("登錄成功,正在跳轉!");
+          store.commit("setUserInfo", result.data);
+          router.push({ path: "/home" });
+        });
+      }else{
+        this.login();
+      }
+
+
+    },
+    async login () {
+      this.$auth.signInWithRedirect({ originalUri: '/' })
+
+    },
+    async logout() {
+      await this.$auth.signOut()
+
+    }
+  }
+}
+</script>
