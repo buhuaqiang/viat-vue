837768971a2c643f1344e8b01c0a3c7c73d48f95
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-06-27 05:20:05 +0000

    修改路径

--- src/extension/basic/cust/Viat_com_custModelBody.vue
+++ src/extension/basic/cust/Viat_com_custModelBody.vue
@@ -219,7 +219,7 @@ export default {
           this.model=false;
         })
       }
-      else if ((path == '/000' && this.formType == 'f') || this.formType=='ext'){//多層級調用
+      else if ((path == '/View_app_power_contract_main' && this.formType == 'f') || this.formType=='ext'){//多層級調用
         this.$emit("onSelect", this.fieldName, rows)
       }else if(path == '/View_app_hp_contract'&& this.formType == 'f'){
         this.$emit("onSelect", this.fieldName, rows)
