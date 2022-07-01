2e6471e441c9f61713382c0d8fdcdb36ef33a84c
jo <jo@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-01 05:14:26 +0000

    1.將選擇 清除按鈕改英文 2.fix clearData寫法, 導致不能清除

--- Vol.Vue3/src/extension/basic/prod/View_com_prod_pop_query.vue
+++ Vol.Vue3/src/extension/basic/prod/View_com_prod_pop_query.vue
@@ -127,10 +127,10 @@
 
                 this.$emit("parentCall", ($parent) => {
                     debugger
-                    if(this.formType=='f'){
+                    if(formType=='f'){
                         $parent.editFormFields[fieldName] = '';
                         $parent.editFormFields[fieldName+'name'] = '';
-                    }else if(this.formType=='s'){
+                    }else if(formType=='s'){
                         $parent.searchFormFields[fieldName] = '';
                         $parent.searchFormFields[fieldName+'name'] ='';
                     }
