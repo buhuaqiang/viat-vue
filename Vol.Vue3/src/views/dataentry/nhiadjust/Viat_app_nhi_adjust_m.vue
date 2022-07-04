691b5065ba2fbedbcd195fdeffd200f75895f687
jo <jo@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-04 08:12:55 +0000

    adjust first

---
+++ Vol.Vue3/src/views/dataentry/nhiadjust/Viat_app_nhi_adjust_m.vue
@@ -0,0 +1,73 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/nhiadjust/Viat_app_nhi_adjust_m.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/dataentry/nhiadjust/Viat_app_nhi_adjust_m.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'nhiadjustm_dbid',
+                footer: "Foots",
+                cnName: '健保價調整主表一',
+                name: 'nhiadjust/Viat_app_nhi_adjust_m',
+                url: "/Viat_app_nhi_adjust_m/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"nhi_batch":"","start_date":"","remarks":""});
+            const editFormOptions = ref([[{"title":"Batch No","required":true,"field":"nhi_batch","type":"text"},
+                               {"title":"Base Date","field":"start_date","type":"date"},
+                               {"title":"Note","field":"remarks","type":"text"}]]);
+            const searchFormFields = ref({});
+            const searchFormOptions = ref([]);
+            const columns = ref([{field:'nhiadjustm_dbid',title:'PKID',type:'guid',width:110,hidden:true,readonly:true,require:true,align:'left'},
+                       {field:'nhi_batch',title:'Batch No',type:'int',width:110,hidden:true,require:true,align:'left'},
+                       {field:'start_date',title:'Base Date',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'status',title:'是否有效,Y:生效/N:未生效/C:移除',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'apply_allw',title:'是否申請庫存價差',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'remarks',title:'Note',type:'string',width:220,hidden:true,align:'left'},
+                       {field:'created_user',title:'建立用戶',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_username',title:'created_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_client',title:'建立者的委託人',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_clientusername',title:'created_clientusername',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_date',title:'建立時間',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'modified_user',title:'最後修改用戶',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_username',title:'modified_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'modified_client',title:'最後修改者的委託人',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_clientusername',title:'modified_clientusername',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'modified_date',title:'最後修改時間',type:'datetime',width:110,hidden:true,align:'left',sort:true}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
---
+++ Vol.Vue3/src/views/dataentry/nhiadjust/View_nhi_adjust.vue
@@ -0,0 +1,76 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/nhiadjust/View_nhi_adjust.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :detail="detail"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :searchFormFields="searchFormFields"
+               :searchFormOptions="searchFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/dataentry/nhiadjust/View_nhi_adjust.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'nhiadjustm_dbid',
+                footer: "Foots",
+                cnName: '健保價調整',
+                name: 'nhiadjust/View_nhi_adjust',
+                url: "/View_nhi_adjust/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"nhi_batch":"","start_date":"","remarks":""});
+            const editFormOptions = ref([[{"title":"Batch No","required":true,"field":"nhi_batch","type":"text"},
+                               {"title":"Base Date","field":"start_date","type":"date"}],
+                              [{"title":"Note","field":"remarks","type":"text"}]]);
+            const searchFormFields = ref({"nhiadjustm_dbid":"","start_date":"","prod_id":"","prod_ename":""});
+            const searchFormOptions = ref([[{"dataKey":"nhiAdjust","data":[],"title":"Batch No","field":"nhiadjustm_dbid","type":"select"},{"title":"Base Date","field":"start_date","type":"date"},{"title":"Product","field":"prod_id"},{"title":"","field":"prod_ename"}]]);
+            const columns = ref([{field:'nhiadjustm_dbid',title:'Batch No',type:'guid',bind:{ key:'nhiAdjust',data:[]},width:110,hidden:true,require:true,align:'left'},
+                       {field:'nhi_batch',title:'Batch No',type:'int',width:110,require:true,align:'left',sort:true},
+                       {field:'start_date',title:'Base Date',type:'datetime',width:110,align:'left',sort:true},
+                       {field:'status',title:'Status',type:'string',width:110,align:'left'},
+                       {field:'apply_allw',title:'apply_allw',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'remarks',title:'Note',type:'string',width:220,align:'left'},
+                       {field:'created_user',title:'created_user',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_username',title:'created_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_client',title:'created_client',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_clientusername',title:'created_clientusername',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_date',title:'created_date',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'modified_user',title:'modified_user',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_username',title:'modified_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'modified_client',title:'modified_client',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_clientusername',title:'modified_clientusername',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'modified_date',title:'modified_date',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'prod_dbid',title:'prod_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                       {field:'prod_id',title:'Product',type:'string',width:110,hidden:true,require:true,align:'left'},
+                       {field:'prod_ename',title:'prod_ename',type:'string',width:110,hidden:true,align:'left'}]);
+            const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                searchFormFields,
+                searchFormOptions,
+                columns,
+                detail,
+            };
+        },
+    });
+</script>
