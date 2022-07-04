a5954b5d7677973ab63cec7a61265f13bce3c8fc
fangqing <fangqing@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-04 06:55:27 +0000

    full  allowance

---
+++ src/extension/dataentry/fullallowance/view_full_allowance_ModelBody.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="vol-tabs">
+    <el-tabs type="border-card" style="
+        min-height: 370px;
+        box-shadow: none;
+        border-bottom: 0;
+        border-top: 1px solid #eee;
+      ">
+      <el-tab-pane>
+        <template #label>
+          <span><i class="el-icon-date"></i> Sharing</span>
+        </template>
+        <allowance-sharing ref="allowanceSharing"></allowance-sharing>
+       <!-- <div>
+          <allowance-sharing ref="allowanceSharing"></allowance-sharing>
+        </div>-->
+       <!-- <vol-table ref="table1">
+          <allowance-sharing ref="allowanceSharing"></allowance-sharing>
+        </vol-table>-->
+      </el-tab-pane>
+      <el-tab-pane>
+        <template #label>
+          <span><i class="el-icon-date"></i> Reverse</span>
+        </template>
+
+        <!--<div>
+          <allowance-reverse ref="allowanceReverse"></allowance-reverse>
+        </div>-->
+
+        <vol-table ref="table2">
+          <allowance-reverse ref="allowanceReverse"></allowance-reverse>
+        </vol-table>
+      </el-tab-pane>
+    </el-tabs>
+  </div>
+
+
+</template>
+<script>
+//开发一对多从表需要参照voltable与viewgrid组件api
+import VolTable from "@/components/basic/VolTable.vue";
+import custmModelBody from  '../../basic/cust/Viat_com_custModelBody.vue'; //'@/extension/basic/cust/Viat_com_custModelBody'
+import allowanceSharing from   "@/views/dataentry/fullallowance/View_full_allowance_sharing.vue";
+import allowanceReverse from "@/views/dataentry/fullallowance/View_full_allowance_reverse.vue";
+export default {
+  components: {allowanceSharing,allowanceReverse},
+  data() {
+
+  },
+  methods: {
+    //如果要获取页面的参数请使用 this.$emit("parentCall",见下面的使用方法
+    //shareTable查询显示
+
+    modelOpen() {
+      let $parent;
+      //获取生成页面viewgrid的对象
+      this.$emit("parentCall", ($this) => {
+        $parent = $this;
+      });
+      $parent.boxOptions.height=600;
+      $parent.boxOptions.width=1500;
+      this.$refs.table1.load();
+      this.$refs.table2.load();
+    }
+
+  },
+};
+</script>
+<style lang="less" scoped>
+.vol-tabs {
+  background: white;
+}
+.view-header .desc-text{
+  display: none;
+}
+
+</style>
---
+++ src/views/dataentry/fullallowance/View_full_allowance_reverse.vue
@@ -0,0 +1,94 @@
+<!--
+*Author：jxx
+ *Contact：283591387@qq.com
+ *代码由框架生成,任何更改都可能导致被代码生成器覆盖
+ *业务请在@/extension/system/fullallowance/View_full_allowance_reverse.js此处编写
+ -->
+<template>
+    <view-grid ref="grid"
+               :columns="columns"
+               :editFormFields="editFormFields"
+               :editFormOptions="editFormOptions"
+               :table="table"
+               :extend="extend">
+    </view-grid>
+</template>
+<script>
+    import extend from "@/extension/dataentry/fullallowance/View_full_allowance_reverse.js";
+    import { ref, defineComponent } from "vue";
+    export default defineComponent({
+        setup() {
+            const table = ref({
+                key: 'allw_sum_dbid',
+                footer: "Foots",
+                cnName: '',
+                name: 'fullallowance/View_full_allowance_reverse',
+                url: "/View_full_allowance_reverse/",
+                sortName: "created_date"
+            });
+            const editFormFields = ref({"cust_id":"","prod_id":"","trans_date":"","invoice_no":"","qty":"","amount":"","remarks":""});
+            const editFormOptions = ref([[{"title":"Customer ID","field":"cust_id"}],
+                              [{"title":"Product ID","field":"prod_id"},
+                               {"title":"TransDate","required":true,"field":"trans_date"}],
+                              [{"title":"Invoice No","field":"invoice_no"}],
+                              [{"title":"Qty","field":"qty","type":"decimal"}],
+                              [{"title":"Amount","field":"amount","type":"decimal"}],
+                              [{"title":"Remarks","field":"remarks"}]]);
+            //const searchFormFields = ref({});
+            //const searchFormOptions = ref([]);
+            const columns = ref([{field:'cust_id',title:'Customer ID',type:'string',width:130,align:'left',sort:true},
+                       {field:'cust_name',title:'Customer',type:'string',width:120,align:'left'},
+                       {field:'prod_id',title:'Product ID',type:'string',width:110,align:'left'},
+                       {field:'prod_ename',title:'Product',type:'string',width:110,align:'left'},
+                       {field:'amount',title:'Amount',type:'decimal',width:90,align:'left'},
+                       {field:'qty',title:'Qty',type:'decimal',width:90,align:'left'},
+                       {field:'invoice_date',title:'Invoice Date',type:'datetime',width:130,align:'left',sort:true},
+                       {field:'invoice_no',title:'Invoice No',type:'string',width:110,align:'left'},
+                       {field:'remarks',title:'Remarks',type:'string',width:150,align:'left'},
+                       {field:'modified_user',title:'Modify_user',type:'int',width:110,align:'left'},
+                       {field:'modified_date',title:'Modify Date',type:'datetime',width:150,align:'left',sort:true},
+                       {field:'allw_sum_dbid',title:'列名allw_sum_dbid',type:'guid',width:110,hidden:true,require:true,align:'left'},
+                       {field:'entity',title:'列名entity',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'division',title:'列名division',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'action_type',title:'列名action_type',type:'string',width:110,hidden:true,require:true,align:'left'},
+                       {field:'allw_type',title:'列名allw_type',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'year',title:'列名year',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'period',title:'列名period',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'hpcont_dbid',title:'列名hpcont_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                       {field:'prod_dbid',title:'列名prod_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                       {field:'cust_dbid',title:'列名cust_dbid',type:'guid',width:110,hidden:true,align:'left'},
+                       {field:'dist_id',title:'列名dist_id',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'trans_date',title:'TransDate',type:'datetime',width:110,require:true,align:'left',sort:true},
+                       {field:'status',title:'列名status',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'o_contract_no',title:'列名o_contract_no',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_user',title:'列名created_user',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_username',title:'列名created_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_client',title:'列名created_client',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'created_clientusername',title:'列名created_clientusername',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'created_date',title:'列名created_date',type:'datetime',width:110,hidden:true,align:'left',sort:true},
+                       {field:'modified_username',title:'列名modified_username',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'modified_client',title:'列名modified_client',type:'int',width:110,hidden:true,align:'left'},
+                       {field:'modified_clientusername',title:'列名modified_clientusername',type:'string',width:110,hidden:true,align:'left'},
+                       {field:'percent',title:'列名percent',type:'decimal',width:110,hidden:true,align:'left'}]);
+            /*const detail = ref({
+                cnName: "#detailCnName",
+                table: "#detailTable",
+                columns: [],
+                sortName: "",
+                key: ""
+            });*/
+            return {
+                table,
+                extend,
+                editFormFields,
+                editFormOptions,
+                columns,
+            };
+        },
+    });
+</script>
+<style lang="less" scoped>
+.desc-text{
+        display: none!important;
+    }
+</style>
