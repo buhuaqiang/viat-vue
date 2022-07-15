57ea2897cb416cb0dd3e63098aa2e2ad6e4a2eeb
ming <ming@4cf86e6a-6f6d-0b46-ae44-afab4e70ef7a>
2022-07-15 08:54:30 +0000

    [Fix]price distributor mapping new/edit視窗寬度調整

--- Vol.Vue3/src/extension/price/price/View_price_distributor_mapping.js
+++ Vol.Vue3/src/extension/price/price/View_price_distributor_mapping.js
@@ -39,6 +39,7 @@ let extension = {
       this.labelWidth = 180;
       //示例：设置修改新建、编辑弹出框字段标签的长度
       this.boxOptions.labelWidth = 180;
+      this.boxOptions.width=1500;
       this.setFiexdSearchForm(true);
       this.load = false;
 
